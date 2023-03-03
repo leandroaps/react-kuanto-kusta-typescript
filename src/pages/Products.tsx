2bca4070d95480a631110b997057e34d97f7b8eb
leandroaps <leandroaps@gmail.com>
2023-03-02 21:19:34 -0300

    feat: 🎸 Products Page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import '../styles/Products.scss';
+
+import React, { useEffect, useState } from 'react';
 
 const Products: React.FC = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    void fetch('https://fakestoreapi.com/products')
+      .then(async (res) => await res.json())
+      .then((json) => {
+        console.log(json)
+      })
+  }, [])
+
+  useEffect(() => {
+    void fetch('https://fakestoreapi.com/products')
+      .then(async (res) => await res.json())
+      .then((json) => {
+        setProducts(json)
+      })
+
+    return () => {
+      console.log(products)
+    }
+  }, [])
+
   return (
     <>
       <h1>PRODUCTS</h1>
+      {products?.map((product: any): any => (
+        <div className="card" key={product.id}>
+          <img
+            src={product.image}
+            className="card-img-top"
+            alt={product.description}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{product.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">
+              {product.category}
+            </h6>
+            <span className="badge bg-secondary">
+              Price US$ {product.price}
+            </span>
+            <p className="card-text">{product.description}</p>
+            <a href="#" className="btn btn-primary">
+              Details
+            </a>
+            <a href="#" className="btn btn-primary">
+              Add to Cart
+            </a>
+          </div>
+        </div>
+      ))}
     </>
   )
 }
